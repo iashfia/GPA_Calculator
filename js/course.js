4acb796af89bb6102e296788612e18827385f7e8
iashfia <ibnatashfia@gmail.com>
2022-06-22 22:22:55 -0400

    update

---
+++ js/course.js
@@ -0,0 +1,118 @@
+/* 
+
+GPA Calculator by Ashfia
+https://github.com/iashfia/
+
+*/
+
+
+var courseName = "Neuroscience";
+
+var courseUnits = [{
+    year: '1',
+    sems: [{
+        sem: '1',
+        subs: [{
+            name: 'Fundamentals of Computing',
+            id: 'SENG 11213'
+        }, {
+            name: 'Programming Concepts',
+            id: 'SENG 11223'
+        }, {
+            name: 'Engineering Foundation',
+            id: 'SENG 11232'
+        }, {
+            name: 'Statistics',
+            id: 'SENG 11243'
+        }, {
+            name: 'Discreet Mathematics for Computing I A',
+            id: 'PMAT 11212'
+        }, {
+            name: 'English for Professionals',
+            id: 'DELT 11212'
+        }]
+    },
+    {
+        sem: '2',
+        subs: [{
+            name: 'Data Structures and Algorithms',
+            id: 'SENG 12213'
+        }, {
+            name: 'Database Design and Development',
+            id: 'SENG 12223'
+        }, {
+            name: 'Object Oriented Programming',
+            id: 'SENG 12233'
+        }, {
+            name: 'Management for Software Engineering I',
+            id: 'SENG 12242'
+        }, {
+            name: 'Discreet Mathematics for Computing Ii B',
+            id: 'PMAT 12212'
+        }, {
+            name: 'Communication Skills for Professionals',
+            id: 'DELT 12312'
+        }]
+    }
+    ]
+},
+ /*
+{
+    year: '2',
+    sems: [{
+        sem: '3',
+        subs: [{
+            name: 'Computer Architecture and Operating Systems',
+            id: 'SENG 21213'
+        }, {
+            name: 'Software Constructions',
+            id: 'SENG 21222'
+        }, {
+            name: 'Requirement Engineering',
+            id: 'SENG 21233'
+        }, {
+            name: 'Software Modelling',
+            id: 'SENG 21243'
+        }, {
+            name: 'Web Application Development',
+            id: 'SENG 21253'
+        }, {
+            name: 'Management for Software Engineering II',
+            id: 'SENG 21272'
+        }, {
+            name: 'Computer Networks',
+            id: 'SENG 24213'
+        }, {
+            name: 'Interactive Application Development',
+            id: 'SENG 21263',
+            type: 'o'
+        }]
+    },
+    {
+        sem: '4',
+        subs: [{
+            name: 'Software Architecture and Design',
+            id: 'SENG 22212'
+        }, {
+            name: 'Human Computer Interaction',
+            id: 'SENG 22223'
+        }, {
+            name: 'Software Verification and Validation',
+            id: 'SENG 22233'
+        }, {
+            name: 'Mobile Application Development',
+            id: 'SENG 22243'
+        }, {
+            name: 'Embedded Systems Development',
+            id: 'SENG 22253',
+            type: 'o'
+        }, {
+            name: 'Mathematical Methods',
+            id: 'PMAT 22213',
+            type: 'o'
+        }]
+    }
+    ]
+},
+*/
+];
